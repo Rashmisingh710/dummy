7ba58c32a2979e5bd13e5b6509f14329c6e9efb7
Rashmi_Singh10 <rashmi.r081023@gmail.com>
2024-11-14 18:22:28 +0530

    Add files via upload

---
+++ BankAccount.java
@@ -0,0 +1,19 @@
+package Example;
+
+public class BankAccount {
+    //ac no,branch name ,branch 
+    String BranchName;
+    String Branch;
+    double Customer_Balannce;  
+     public static void main(String[] args) {
+        BankAccount ban=new BankAccount();
+        ban.BranchName="Sbi";
+        ban.Branch="";
+
+        System.out.println(ban.BranchName);
+        System.err.println(ban.Branch);
+        System.err.println(ban.Branch);
+        
+     }
+
+}
